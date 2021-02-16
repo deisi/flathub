14843289b5a8642a50d0f505611962d95297b96c
Malte Deiseroth <mdeiseroth88@gmail.com>
2021-02-16 13:52:38 +0100

    Move exit by 1 for testing

--- update.sh
+++ update.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-exit 0
-
 GIT_USER_NAME=$(git config user.name)
 GIT_USER_EMAIL=$(git config user.email)
 
 CWD=$(pwd)
 
+exit 0
 podman run --rm --privileged \
     -v $HOME:$HOME:rslave \
     -v $CWD:$CWD:rslave \
