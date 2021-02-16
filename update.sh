82617b7769b9c104edaead1b33bac1545b31ce51
Malte Deiseroth <mdeiseroth88@gmail.com>
2021-02-16 12:34:47 +0100

    Started to work on CI Pipeline

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+GIT_USER_NAME=$(git config user.name)
+GIT_USER_EMAIL=$(git config user.email)
+
+CWD=$(pwd)
+
+podman run --rm --privileged \
+    -v $HOME:$HOME:rslave \
+    -v $CWD:$CWD:rslave \
+    -w $CWD \
+    -e GIT_AUTHOR_NAME="$GIT_USER_NAME" \
+    -e GIT_COMMITTER_NAME="$GIT_USER_NAME" \
+    -e GIT_AUTHOR_EMAIL="$GIT_USER_EMAIL" \
+    -e GIT_COMMITTER_EMAIL="$GIT_USER_EMAIL" \
+    -it ghcr.io/flathub/flatpak-external-data-checker \
+    info.portfolio_performance.PortfolioPerformance.json
+
+# If broken write email
+
+# 
+
+
